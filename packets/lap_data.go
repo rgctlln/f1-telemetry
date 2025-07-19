d85f5817ec253ad9719e5098e58f66d32f11ef50
rgctlln <408136@niuitmo.ru>
2025-07-20 02:06:35 +0300

    small updates added(i'm nearly parsing x,y,z to the file)

--- packets/lap_data.go
+++ packets/lap_data.go
@@ -7,48 +7,48 @@ import (
 
 type PacketLapData struct {
 	Header  PacketHeader
-	LapData [22]lapData
+	LapData [22]LapData
 }
 
-type lapData struct {
-	lastLapTimeInMS              uint32  // Last lap time in milliseconds
-	currentLapTimeInMS           uint32  // Current time around the lap in milliseconds
-	sector1TimeInMS              uint16  // Sector 1 time milliseconds part
-	sector1TimeMinutesPart       uint8   // Sector 1 whole minute part
-	sector2TimeInMS              uint16  // Sector 2 time milliseconds part
-	sector2TimeMinutesPart       uint8   // Sector 2 whole minute part
-	deltaToCarInFrontMSPart      uint16  // Time delta to car in front milliseconds part
-	deltaToCarInFrontMinutesPart uint8   // Time delta to car in front whole minute part
-	deltaToRaceLeaderMSPart      uint16  // Time delta to race leader milliseconds part
-	deltaToRaceLeaderMinutesPart uint8   // Time delta to race leader whole minute part
-	lapDistance                  float32 // Distance around current lap in metres
-	totalDistance                float32 // Total distance travelled in session in metres
-	safetyCarDelta               float32 // Delta in seconds for safety car
-	carPosition                  uint8   // Car race position
-	currentLapNum                uint8   // Current lap number
-	pitStatus                    uint8   // 0 = none, 1 = pitting, 2 = in pit area
-	numPitStops                  uint8   // Number of pit stops taken in this race
-	sector                       uint8   // 0 = sector1, 1 = sector2, 2 = sector3
-	currentLapInvalid            uint8   // 0 = valid, 1 = invalid
-	penalties                    uint8   // Accumulated time penalties in seconds
-	totalWarnings                uint8   // Accumulated number of warnings issued
-	cornerCuttingWarnings        uint8   // Number of corner cutting warnings issued
-	numUnservedDriveThroughPens  uint8   // Drive through penalties left to serve
-	numUnservedStopGoPens        uint8   // Stop & go penalties left to serve
-	gridPosition                 uint8   // Grid position at race start
-	driverStatus                 uint8   // 0=garage,1=flying lap,2=in lap,3=out lap,4=on track
-	resultStatus                 uint8   // 0=invalid,1=inactive,2=active,3=finished,4=dnf,5=dsq,6=not classified,7=retired
-	pitLaneTimerActive           uint8   // Pit lane timing: 0=inactive,1=active
-	pitLaneTimeInLaneInMS        uint16  // Time spent in pit lane in ms
-	pitStopTimerInMS             uint16  // Time of the actual pit stop in ms
-	pitStopShouldServePen        uint8   // Whether the car should serve a penalty at this stop
-	speedTrapFastestSpeed        float32 // Fastest speed through speed trap in km/h
-	speedTrapFastestLap          uint8   // Lap number fastest speed was achieved (255 = not set)
+type LapData struct {
+	LastLapTimeInMS              uint32  // Last lap time in milliseconds
+	CurrentLapTimeInMS           uint32  // Current time around the lap in milliseconds
+	Sector1TimeInMS              uint16  // Sector 1 time milliseconds part
+	Sector1TimeMinutesPart       uint8   // Sector 1 whole minute part
+	Sector2TimeInMS              uint16  // Sector 2 time milliseconds part
+	Sector2TimeMinutesPart       uint8   // Sector 2 whole minute part
+	DeltaToCarInFrontMSPart      uint16  // Time delta to car in front milliseconds part
+	DeltaToCarInFrontMinutesPart uint8   // Time delta to car in front whole minute part
+	DeltaToRaceLeaderMSPart      uint16  // Time delta to race leader milliseconds part
+	DeltaToRaceLeaderMinutesPart uint8   // Time delta to race leader whole minute part
+	LapDistance                  float32 // Distance around current lap in metres
+	TotalDistance                float32 // Total distance travelled in session in metres
+	SafetyCarDelta               float32 // Delta in seconds for safety car
+	CarPosition                  uint8   // Car race position
+	CurrentLapNum                uint8   // Current lap number
+	PitStatus                    uint8   // 0 = none, 1 = pitting, 2 = in pit area
+	NumPitStops                  uint8   // Number of pit stops taken in this race
+	Sector                       uint8   // 0 = sector1, 1 = sector2, 2 = sector3
+	CurrentLapInvalid            uint8   // 0 = valid, 1 = invalid
+	Penalties                    uint8   // Accumulated time penalties in seconds
+	TotalWarnings                uint8   // Accumulated number of warnings issued
+	CornerCuttingWarnings        uint8   // Number of corner cutting warnings issued
+	NumUnservedDriveThroughPens  uint8   // Drive through penalties left to serve
+	NumUnservedStopGoPens        uint8   // Stop & go penalties left to serve
+	GridPosition                 uint8   // Grid position at race start
+	DriverStatus                 uint8   // 0=garage,1=flying lap,2=in lap,3=out lap,4=on track
+	ResultStatus                 uint8   // 0=invalid,1=inactive,2=active,3=finished,4=dnf,5=dsq,6=not classified,7=retired
+	PitLaneTimerActive           uint8   // Pit lane timing: 0=inactive,1=active
+	PitLaneTimeInLaneInMS        uint16  // Time spent in pit lane in ms
+	PitStopTimerInMS             uint16  // Time of the actual pit stop in ms
+	PitStopShouldServePen        uint8   // Whether the car should serve a penalty at this stop
+	SpeedTrapFastestSpeed        float32 // Fastest speed through speed trap in km/h
+	SpeedTrapFastestLap          uint8   // Lap number fastest speed was achieved (255 = not set)
 }
 
 func ParseLapDataPacket(data []byte) PacketLapData {
 	header := ParseHeader(data)
-	var lapDataArr [22]lapData
+	var lapDataArr [22]LapData
 
 	const (
 		base      = 29
@@ -57,48 +57,48 @@ func ParseLapDataPacket(data []byte) PacketLapData {
 
 	for i := 0; i < 22; i++ {
 		offset := base + i*entrySize
-		lapDataArr[i] = lapData{
-			lastLapTimeInMS:    binary.LittleEndian.Uint32(data[offset+0 : offset+4]),
-			currentLapTimeInMS: binary.LittleEndian.Uint32(data[offset+4 : offset+8]),
+		lapDataArr[i] = LapData{
+			LastLapTimeInMS:    binary.LittleEndian.Uint32(data[offset+0 : offset+4]),
+			CurrentLapTimeInMS: binary.LittleEndian.Uint32(data[offset+4 : offset+8]),
 
-			sector1TimeInMS:        binary.LittleEndian.Uint16(data[offset+8 : offset+10]),
-			sector1TimeMinutesPart: data[offset+10],
+			Sector1TimeInMS:        binary.LittleEndian.Uint16(data[offset+8 : offset+10]),
+			Sector1TimeMinutesPart: data[offset+10],
 
-			sector2TimeInMS:        binary.LittleEndian.Uint16(data[offset+11 : offset+13]),
-			sector2TimeMinutesPart: data[offset+13],
+			Sector2TimeInMS:        binary.LittleEndian.Uint16(data[offset+11 : offset+13]),
+			Sector2TimeMinutesPart: data[offset+13],
 
-			deltaToCarInFrontMSPart:      binary.LittleEndian.Uint16(data[offset+14 : offset+16]),
-			deltaToCarInFrontMinutesPart: data[offset+16],
+			DeltaToCarInFrontMSPart:      binary.LittleEndian.Uint16(data[offset+14 : offset+16]),
+			DeltaToCarInFrontMinutesPart: data[offset+16],
 
-			deltaToRaceLeaderMSPart:      binary.LittleEndian.Uint16(data[offset+17 : offset+19]),
-			deltaToRaceLeaderMinutesPart: data[offset+19],
+			DeltaToRaceLeaderMSPart:      binary.LittleEndian.Uint16(data[offset+17 : offset+19]),
+			DeltaToRaceLeaderMinutesPart: data[offset+19],
 
-			lapDistance:    math.Float32frombits(binary.LittleEndian.Uint32(data[offset+20 : offset+24])),
-			totalDistance:  math.Float32frombits(binary.LittleEndian.Uint32(data[offset+24 : offset+28])),
-			safetyCarDelta: math.Float32frombits(binary.LittleEndian.Uint32(data[offset+28 : offset+32])),
+			LapDistance:    math.Float32frombits(binary.LittleEndian.Uint32(data[offset+20 : offset+24])),
+			TotalDistance:  math.Float32frombits(binary.LittleEndian.Uint32(data[offset+24 : offset+28])),
+			SafetyCarDelta: math.Float32frombits(binary.LittleEndian.Uint32(data[offset+28 : offset+32])),
 
-			carPosition:                 data[offset+32],
-			currentLapNum:               data[offset+33],
-			pitStatus:                   data[offset+34],
-			numPitStops:                 data[offset+35],
-			sector:                      data[offset+36],
-			currentLapInvalid:           data[offset+37],
-			penalties:                   data[offset+38],
-			totalWarnings:               data[offset+39],
-			cornerCuttingWarnings:       data[offset+40],
-			numUnservedDriveThroughPens: data[offset+41],
-			numUnservedStopGoPens:       data[offset+42],
-			gridPosition:                data[offset+43],
-			driverStatus:                data[offset+44],
-			resultStatus:                data[offset+45],
-			pitLaneTimerActive:          data[offset+46],
+			CarPosition:                 data[offset+32],
+			CurrentLapNum:               data[offset+33],
+			PitStatus:                   data[offset+34],
+			NumPitStops:                 data[offset+35],
+			Sector:                      data[offset+36],
+			CurrentLapInvalid:           data[offset+37],
+			Penalties:                   data[offset+38],
+			TotalWarnings:               data[offset+39],
+			CornerCuttingWarnings:       data[offset+40],
+			NumUnservedDriveThroughPens: data[offset+41],
+			NumUnservedStopGoPens:       data[offset+42],
+			GridPosition:                data[offset+43],
+			DriverStatus:                data[offset+44],
+			ResultStatus:                data[offset+45],
+			PitLaneTimerActive:          data[offset+46],
 
-			pitLaneTimeInLaneInMS: binary.LittleEndian.Uint16(data[offset+47 : offset+49]),
-			pitStopTimerInMS:      binary.LittleEndian.Uint16(data[offset+49 : offset+51]),
-			pitStopShouldServePen: data[offset+51],
+			PitLaneTimeInLaneInMS: binary.LittleEndian.Uint16(data[offset+47 : offset+49]),
+			PitStopTimerInMS:      binary.LittleEndian.Uint16(data[offset+49 : offset+51]),
+			PitStopShouldServePen: data[offset+51],
 
-			speedTrapFastestSpeed: math.Float32frombits(binary.LittleEndian.Uint32(data[offset+52 : offset+56])),
-			speedTrapFastestLap:   data[offset+56],
+			SpeedTrapFastestSpeed: math.Float32frombits(binary.LittleEndian.Uint32(data[offset+52 : offset+56])),
+			SpeedTrapFastestLap:   data[offset+56],
 		}
 	}
 
--- tracks/track_creator.go
+++ tracks/track_creator.go
@@ -6,15 +6,13 @@ import (
 	"log"
 	"net"
 	"os"
+	"sync"
 )
 
-// GetTrackCoordinates слушает conn и дописывает
-// WorldPositionX,Y,Z первой машины в файл ./tracks/<trackName>_2024_racingline.txt
+// Listens conn and gets WorldPositionX,Y,Z and writes to ./tracks/<trackName>_2024_racingline.txt
 func GetTrackCoordinates(conn *net.UDPConn, trackName string) {
-	// Открываем (или создаём) файл в режиме append
 	path := fmt.Sprintf("./tracks/generated_tracks/%s_2024_racingline.txt", trackName)
-	f, err := os.OpenFile(path,
-		os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatalf("failed to open %s: %v", path, err)
 	}
@@ -24,28 +22,67 @@ func GetTrackCoordinates(conn *net.UDPConn, trackName string) {
 		}
 	}()
 
-	buf := make([]byte, 2048)
+	buffer := make([]byte, 2048)
+	once := sync.Once{}
+
 	for {
-		// читаем один UDP-пакет
-		n, _, err := conn.ReadFromUDP(buf)
+		ln, UDPaddr, err := conn.ReadFromUDP(buffer)
 		if err != nil {
-			log.Printf("read error: %v", err)
-			continue
+			log.Fatalf("Error reading from UDP: %v", err)
 		}
 
-		// парсим MotionPacket
-		motion := packets.ParseMotionPacket(buf[:n])
-		playerCarIndex := motion.Header.PlayerCarIndex
+		once.Do(func() {
+			log.Printf("Read from UDP: %v, trying to get track coordinates.", UDPaddr)
+		})
 
-		// берём координаты первой машины
-		x := motion.CarMotionData[playerCarIndex].WorldPositionX
-		y := motion.CarMotionData[playerCarIndex].WorldPositionY
-		z := motion.CarMotionData[playerCarIndex].WorldPositionZ
+		buffer = buffer[:ln]
+		header := packets.ParseHeader(buffer)
 
-		// дописываем в файл
-		line := fmt.Sprintf("%f,%f,%f\n", x, y, z)
-		if _, err := f.WriteString(line); err != nil {
-			log.Printf("write error: %v", err)
+		if header.PacketId == 2 {
+			lapData := packets.ParseLapDataPacket(buffer)
+			if lapData.LapData[header.PlayerCarIndex].CurrentLapNum == 2 {
+				goto write
+			}
+		}
+	}
+
+write:
+	startWriting(conn, f)
+}
+
+// This function writes X, Y, Z coordinates of the car on the track
+func startWriting(conn *net.UDPConn, f *os.File) {
+	buffer := make([]byte, 2048)
+
+	for {
+		ln, _, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			log.Fatalf("Error reading from UDP: %v", err)
+		}
+
+		buffer = buffer[:ln]
+		header := packets.ParseHeader(buffer)
+
+		switch {
+		case header.PacketId == 2:
+			lapData := packets.ParseLapDataPacket(buffer)
+			if lapData.LapData[header.PlayerCarIndex].CurrentLapNum == 3 {
+				return
+			}
+		case header.PacketId == 0:
+			motion := packets.ParseMotionPacket(buffer)
+			playerCarIndex := header.PlayerCarIndex
+
+			x := motion.CarMotionData[playerCarIndex].WorldPositionX
+			y := motion.CarMotionData[playerCarIndex].WorldPositionY
+			z := motion.CarMotionData[playerCarIndex].WorldPositionZ
+
+			_, err := fmt.Fprintf(f, "%f,%f,%f\n", x, y, z)
+			if err != nil {
+				log.Printf("Error writing to file %v: %v", f, err)
+			}
+
+			//TODO
 		}
 	}
 }
